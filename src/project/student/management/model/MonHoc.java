a42449a2ec1ee36b1f3b823ad9505bfa07243cbd
duongpt582 <tduong.hust02@gmail.com>
2023-01-08 00:10:32 +0700

    Tạo model/SinhVien.java Tạo model/MonHoc.java

---
+++ src/project/student/management/model/MonHoc.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.student.management.model;
+
+/**
+ *
+ * @author duongpt
+ */
+public class MonHoc {
+    private String tenMonHoc;
+    private String maMonHoc;
+    private String hocKy;
+    private double trongSo;
+    private double diemGK;
+    private double diemCK;
+    private int soTinChi;
+}
